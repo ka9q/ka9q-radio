263a98c6b7c241858c8e7cab2501af0b0e225448
Phil Karn <karn@ka9q.net>
2022-04-17 19:09:32 -0700

    const hardening; clean up timespec operations

--- conf.h
+++
@@ -1,4 +0,0 @@
-#define VARDIR "/var/lib/ka9q-radio/"
-#define LIBDIR "/usr/local/share/ka9q-radio/"
-#define BANDPLAN "bandplan.txt"
-#define ID "id.txt"
--- config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONFIG_H
-#define _CONFIG_H 1
-
-int config_getint(dictionary const *d,char const *section,char const *key,int def);
-double config_getdouble(dictionary const *d,char const *section,char const *key,double def);
-float config_getfloat(dictionary const *d,char const *section,char const *key,float def);
-int config_getboolean(dictionary const *d,char const *section,char const *key,int def);
-char const *config_getstring(dictionary const *d,char const *section,char const *key,char const *def);
-#endif
