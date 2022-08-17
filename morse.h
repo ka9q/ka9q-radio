78af6a15486d3809bbdb5b6744c57641bc084ed6
Phil Karn <karn@ka9q.net>
2022-08-17 14:51:25 -0700

    initial commit

--- morse.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MORSE_H
-#define _MORSE_H 1
-#include <stdint.h>
-#include <wchar.h>
-#include <wctype.h>
-
-int encode_morse_char(int16_t *samples,wint_t c);
-int init_morse(float const speed,float const pitch,float const level,float const samprate);
-#endif
