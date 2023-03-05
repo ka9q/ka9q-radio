a4f957de06a8cc971e70ec2f47ab9485180e2233
SoftwareToolworks <123347817+SoftwareToolworks@users.noreply.github.com>
2023-03-05 13:17:30 -0600

    Add DEFAULT_SOURCE Define
    Without this define on linux, macros like NI_MAXHOST don't get defined

--- multicast.c
+++ multicast.c
@@ -1,6 +1,7 @@
 // $Id: multicast.c,v 1.91 2022/10/25 01:57:52 karn Exp $
 // Multicast socket and RTP utility routines
 // Copyright 2018 Phil Karn, KA9Q
+#define _DEFAULT_SOURCE
 
 #include <stdio.h>
 #include <unistd.h>
