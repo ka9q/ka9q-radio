0c529fc7343976fdc4e4f30e0d7ed19c75e70e19
Phil Karn <karn@ka9q.net>
2023-08-31 14:49:00 -0700

    allow polling for responses with interval and count

--- metadump.c
+++ metadump.c
@@ -23,35 +23,84 @@
 #include <netdb.h>
 #include <locale.h>
 #include <sysexits.h>
+#include <errno.h>
+#include <poll.h>
+#include <getopt.h>
 
 #include "misc.h"
 #include "multicast.h"
 #include "status.h"
 
-int Status_sock;
+
 
 const char *App_path;
 int Verbose,Dump;
 bool Newline;
+float Interval = 0.1;
+int Control_sock;
+int Status_sock;
+int Count;
+char *Radio;
+uint32_t Ssrc;
+int IP_tos;
 
 char Locale[256] = "en_US.UTF-8";
 int Mcast_ttl = 5;
+char Optstring[] = "s:c:i:vnr:l:";
+struct option Options[] = {
+  {"ssrc", required_argument, NULL, 's'},
+  {"count", required_argument, NULL, 'c'},
+  {"interval", required_argument, NULL, 'i'},
+  {"verbose", no_argument, NULL, 'v'},  
+  {"newline", no_argument, NULL, 'n'},
+  {"radio", required_argument, NULL, 'r'},
+  {"locale", required_argument, NULL, 'l'},
+
+  {NULL, 0, NULL, 0},
+};
+
+void usage(void);
+
 
 int main(int argc,char *argv[]){
   App_path = argv[0];
   int c;
 
-  while((c = getopt(argc,argv,"vn")) != -1){
+  while((c = getopt(argc,argv,"s:c:i:vnr:")) != -1){
     switch(c){
+    case 's':
+      Ssrc = strtol(optarg,NULL,0);
+      break;
+    case 'c':
+      Count = strtol(optarg,NULL,0);
+      break;
+    case 'i':
+      Interval = strtod(optarg,NULL);
+      break;
     case 'v':
       Verbose++;
       break;
     case 'n':
       Newline = true;
       break;
+    case 'r':
+      Radio = optarg;
+      break;
+    case 'l':
+      strlcpy(Locale,optarg,sizeof(Locale));
+      break;
+     default:
+      usage();
+      break;
     }
   }
-
+  if(Radio == NULL){
+    if(argc <= optind){
+      usage();
+      exit(1);
+    }
+    Radio = argv[optind];
+  }
   {
     char const * const cp = getenv("LANG");
     if(cp != NULL){
@@ -59,27 +108,77 @@ int main(int argc,char *argv[]){
     }
   }
   setlocale(LC_ALL,Locale); // Set either the hardwired default or the value of $LANG if it exists
-  fprintf(stdout,"Listening to %s\n",argv[optind]);
   struct sockaddr_storage sock;
   char iface[1024];
   iface[0] = '\0';
 
-  resolve_mcast(argv[optind],&sock,DEFAULT_STAT_PORT,iface,sizeof(iface));
-  fprintf(stdout,"Interface: %s\n",iface);
+  if(Verbose)
+    fprintf(stdout,"Resolving %s\n",Radio);
+  resolve_mcast(Radio,&sock,DEFAULT_STAT_PORT,iface,sizeof(iface));
+  if(Verbose){
+    char result[1024];
+    fprintf(stdout,"Listening on %s\n",formataddr(result,sizeof(result),&sock));
+  }
+
   Status_sock = listen_mcast(&sock,iface);
   if(Status_sock < 0){
     fprintf(stdout,"Can't set up multicast input\n");
     exit(EX_IOERR);
   }
-
-  for(;;){
-    uint8_t buffer[8192];
-
-    memset(buffer,0,sizeof(buffer));
+  if(Count != 0 || Interval != 0){
+    if(Verbose)
+      fprintf(stdout,"Connecting\n");
+    Control_sock = connect_mcast(&sock,iface,Mcast_ttl,IP_tos);
+    if(Control_sock == -1){
+      fprintf(stdout,"Can't open cmd socket to radio control channel %s: %s\n",Radio,strerror(errno));
+      exit(EX_IOERR);
+    }
+  }
+  int status_packets = 0;
+  if(Verbose){
+    fprintf(stdout,"Polling interval %'llu nanoseconds\n",(long long)(Interval * BILLION));
+  }
+  long long last_command_time = 0;
+  while(Count == 0 || status_packets < Count){ // Count = 0 means infinite
+    // Begin polling SSRC to ensure the multicast group is up and radiod is listening
+    if(Ssrc != 0 && Control_sock > 2){
+      // Time for a rate-limited poll?
+      if(gps_time_ns() >= last_command_time + (long long)(Interval* BILLION)){
+	uint8_t cmd_buffer[9000];
+	uint8_t *bp = cmd_buffer;
+	*bp++ = 1; // Generate command packet
+	uint32_t sent_tag = arc4random();
+	encode_int(&bp,COMMAND_TAG,sent_tag);
+	encode_int(&bp,OUTPUT_SSRC,Ssrc);
+	encode_eol(&bp);
+	int cmd_len = bp - cmd_buffer;
+	if(send(Control_sock, cmd_buffer, cmd_len, 0) != cmd_len)
+	  perror("command send");
+	
+	last_command_time = gps_time_ns();
+	if(Verbose)
+	  fprintf(stdout,"Command sent\n");
+      }
+      // Look for response
+      // Set 100 ms timeout for response
+      struct pollfd fds[1];
+      fds[0].fd = Status_sock;
+      fds[0].events = POLLIN;
+      int event = poll(fds,1,100);
+      if(event == 0)
+	continue; // Timeout; go back and resend
+      
+      if(event < 0){
+	fprintf(stdout,"poll error: %s\n",strerror(errno));
+	exit(1);
+      }      
+    }
+    uint8_t buffer[9000];
     struct sockaddr_storage source;
     socklen_t len = sizeof(source);
     int length = recvfrom(Status_sock,buffer,sizeof(buffer),0,(struct sockaddr *)&source,&len);
     if(length <= 0){
+      fprintf(stderr,"Recvfrom error %s\n",strerror(errno));
       sleep(1);
       continue;
     }
@@ -91,8 +190,15 @@ int main(int argc,char *argv[]){
     fprintf(stdout," %s", cr == STATUS ? "STAT" : cr == CMD ? "CMD" : cr == SSRC_LIST ? "SSRC_LIST" : "unknown");
     dump_metadata(buffer+1,length-1,Newline);
     fflush(stdout);
+    if(cr == STATUS){
+      status_packets++; // Don't count our own responses
+      last_command_time = gps_time_ns(); // Reset poll timeout
+    }
   }
   exit(EX_OK); // can't reach
 }
 
 
+void usage(void){
+  fprintf(stdout,"%s [-s|--ssrc <ssrc>] [-c|--count n] [-i|--interval f] [-v|--verbose] [-n|--newline] [-l|--locale] [ -r|--radio] control-channel\n",App_path);
+}
