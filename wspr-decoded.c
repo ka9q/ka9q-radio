8885c8812361b634e66800a28af16ecd9d6f5f9b
Phil Karn <karn@ka9q.net>
2023-10-16 17:54:29 -0700

    lower debugging

--- wspr-decoded.c
+++ wspr-decoded.c
@@ -99,7 +99,7 @@ struct session *create_session(struct rtp_header *);
 void close_session(struct session **p);
 
 int main(int argc,char *argv[]){
-#if 1
+#if 0
   for(int i=0; i < argc; i++)
     fprintf(stderr," [%d]%s",i,argv[i]);
   fprintf(stderr,"\n");
