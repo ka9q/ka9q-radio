f2778838ae5525042dc6ee35c3b224c23a9d1886
Phil Karn <karn@ka9q.net>
2024-03-16 09:29:17 -0700

    initial commit

---
+++ avahi.h
@@ -0,0 +1,29 @@
+#ifndef _AVAHI_H
+#include <avahi-client/client.h>
+#include <avahi-client/publish.h>
+#include <avahi-common/alternative.h>
+#include <avahi-common/simple-watch.h>
+#include <avahi-common/malloc.h>
+#include <avahi-common/error.h>
+#include <avahi-common/timeval.h>
+
+void *avahi_browser_thread(void *p);
+struct db {
+  struct db *next;
+  struct db *prev;
+  char *name;
+  char *type;
+  char *domain;
+  char *host_name;
+  AvahiAddress address;
+  uint16_t port;
+  char *txt;
+  AvahiLookupResultFlags flags;
+};
+struct db *Database;
+
+
+void *avahi_browser_thread(void *);
+int avahi_start(char const *service_name,char const *service_type,int service_port,char const *dns_name,int base_address,char const *description,void *,int *);
+#define AVAHI_H 1
+#endif
