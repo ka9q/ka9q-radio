c1a4fec951a967f262c5415ae06d55f86335224f
Phil Karn <karn@ka9q.net>
2024-03-19 16:10:59 -0700

    track config; fix format mismatch

--- wd-record.c
+++ wd-record.c
@@ -409,7 +409,7 @@ void input_loop(){
                         int sample_offset_in_next_wav_file = sample_offset_in_current_wav_file - samples_per_minute;
                         if ( verbosity > 2 ) {
                             fprintf(stderr, "input_loop(): after writing %7lld samples to wav file which should have %d samples in it, closing wav file because this new rtp packet is for offset %d in the next wav file.  rtp.timestamp=%u >= sample_number_of_first_sample_in_current_wav_file=%u\n",
-                                    sp->SamplesWritten, samples_per_minute, sample_offset_in_next_wav_file, rtp.timestamp, sample_number_of_first_sample_in_current_wav_file );
+                                    (long long)sp->SamplesWritten, samples_per_minute, sample_offset_in_next_wav_file, rtp.timestamp, sample_number_of_first_sample_in_current_wav_file );
                         }
                         close_session(&sp);
                         sp = NULL;
