24107721940647500f58c6ee12ad70c2e99f6342
Scott Newell <newell@n5tnl.com>
2024-03-30 16:15:53 +0000

    Mod pcmrecord to include center frequency for SDR Console

--- pcmrecord.c
+++ pcmrecord.c
@@ -45,6 +45,31 @@ struct wav {
   int16_t BlockAlign;
   int16_t BitsPerSample;
 
+   // 'auxi' chunk to pass center frequency to SDR Console
+   // http://www.moetronix.com/files/spectravue.pdf had some details on this chunk
+   // and https://sdrplay.com/resources/IQ/ft4.zip
+   // has some .wav files with a center frequency that SDR Console can use
+   char AuxID[4];
+   int32_t AuxSize;
+   int16_t StartYear;
+   int16_t StartMon;
+   int16_t StartDOW;
+   int16_t StartDay;
+   int16_t StartHour;
+   int16_t StartMinute;
+   int16_t StartSecond;
+   int16_t StartMillis;
+   int16_t StopYear;
+   int16_t StopMon;
+   int16_t StopDOW;
+   int16_t StopDay;
+   int16_t StopHour;
+   int16_t StopMinute;
+   int16_t StopSecond;
+   int16_t StopMillis;
+   int32_t CenterFrequency;
+   char AuxUknown[128];
+
   char SubChunk2ID[4];
   int32_t Subchunk2Size;
 };
@@ -60,7 +85,7 @@ struct session {
 
   uint32_t ssrc;               // RTP stream source ID
   struct rtp_state rtp_state;
-  
+
   int type;                    // RTP payload type (with marker stripped)
   int channels;                // 1 (PCM_MONO) or 2 (PCM_STEREO)
   unsigned int samprate;       // implicitly 48 kHz in PCM
@@ -88,6 +113,8 @@ static char const *Locale;
 static int Samprate;
 static int Channels = 1;
 
+static uint32_t CenterFrequency=1115000;
+
 static int Input_fd;
 static struct session *Sessions;
 static int64_t Timeout = 20; // 20 seconds max idle time before file close
@@ -106,15 +133,16 @@ static struct option Options[] = {
   {"mintime", required_argument, NULL, 'm'},
   {"samprate", required_argument, NULL, 'r'},
   {"subdirectories", no_argument, NULL, 's'},
-  {"subdirs", no_argument, NULL, 's'},    
+  {"subdirs", no_argument, NULL, 's'},
   {"timeout", required_argument, NULL, 't'},
   {"verbose", no_argument, NULL, 'v'},
   {"lengthlimit", required_argument, NULL, 'L'},
   {"limit", required_argument, NULL, 'L'},
+  {"frequency", required_argument, NULL, 'f'},
   {"version", no_argument, NULL, 'V'},
   {NULL, no_argument, NULL, 0},
 };
-static char Optstring[] = "c:d:l:m:r:st:vL:V";
+static char Optstring[] = "c:d:l:m:r:st:vL:f:V";
 
 int main(int argc,char *argv[]){
   App_path = argv[0];
@@ -147,7 +175,7 @@ int main(int argc,char *argv[]){
     case 't':
       {
 	char *ptr;
-	int64_t x = strtoll(optarg,&ptr,0); 
+	int64_t x = strtoll(optarg,&ptr,0);
 	if(ptr != optarg)
 	  Timeout = x;
       }
@@ -158,11 +186,14 @@ int main(int argc,char *argv[]){
     case 'L':
       FileLengthLimit = strtof(optarg,NULL);
       break;
+    case 'f':
+       CenterFrequency = strtoul(optarg,NULL,0);
+      break;
     case 'V':
       VERSION();
       exit(EX_OK);
     default:
-      fprintf(stderr,"Usage: %s [-s] [-d directory] [-l locale] [-L maxtime] [-t timeout] [-v] [-m sec] PCM_multicast_address\n",argv[0]);
+      fprintf(stderr,"Usage: %s [-s] [-d directory] [-l locale] [-L maxtime] [-t timeout] [-v] [-m sec] [-f freq] PCM_multicast_address\n",argv[0]);
       exit(EX_USAGE);
       break;
     }
@@ -201,7 +232,7 @@ int main(int argc,char *argv[]){
   signal(SIGINT,closedown);
   signal(SIGKILL,closedown);
   signal(SIGQUIT,closedown);
-  signal(SIGTERM,closedown);        
+  signal(SIGTERM,closedown);
   signal(SIGPIPE,SIG_IGN);
 
   atexit(cleanup);
@@ -244,7 +275,7 @@ static void input_loop(){
       }
       if(size < RTP_MIN_SIZE)
 	continue; // Too small for RTP, ignore
-      
+
       struct rtp_header rtp;
       uint8_t const * const dp = ntoh_rtp(&rtp,buffer);
       if(rtp.pad){
@@ -254,10 +285,10 @@ static void input_loop(){
       }
       if(size <= 0)
 	continue; // Bogus RTP header
-      
+
       int16_t const * const samples = (int16_t *)dp;
       size -= (dp - buffer);
-      
+
       struct session *sp;
       for(sp = Sessions;sp != NULL;sp=sp->next){
 	if(sp->ssrc == rtp.ssrc
@@ -288,7 +319,7 @@ static void input_loop(){
       int const samp_count = size / sizeof(*samples); // number of individual audio samples (not frames)
       int const frame_count = samp_count / sp->channels; // 1 every sample period (e.g., 4 for stereo 16-bit)
       off_t const offset = rtp_process(&sp->rtp_state,&rtp,frame_count); // rtp timestamps refer to frames
-      
+
       // The seek offset relative to the current position in the file is the signed (modular) difference between
       // the actual and expected RTP timestamps. This should automatically handle
       // 32-bit RTP timestamp wraps, which occur every ~1 days at 48 kHz and only 6 hr @ 192 kHz
@@ -332,7 +363,7 @@ static void input_loop(){
     }
   }
 }
- 
+
 static void cleanup(void){
   while(Sessions){
     // Flush and close each write stream
@@ -347,11 +378,11 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
   struct session *sp = calloc(1,sizeof(*sp));
   if(sp == NULL)
     return NULL; // unlikely
-  
+
   memcpy(&sp->sender,sender,sizeof(sp->sender));
   sp->type = rtp->type;
   sp->ssrc = rtp->ssrc;
-  
+
   sp->channels = Channels ? Channels : channels_from_pt(sp->type);
   sp->samprate = Samprate ? Samprate : samprate_from_pt(sp->type);
   if(sp->channels == 0 || sp->samprate == 0){
@@ -359,14 +390,14 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
     return NULL;
   }
   sp->samples_remaining = sp->samprate * FileLengthLimit; // If file is being limited in length
-  
+
   // Create file
   // Should we append to existing files instead? If we try this, watch out for timestamp wraparound
   struct timespec now;
   clock_gettime(CLOCK_REALTIME,&now);
   struct tm const * const tm = gmtime(&now.tv_sec);
   // yyyy-mm-dd-hh:mm:ss so it will sort properly
-  
+
   sp->fp = NULL;
   if(Subdirs){
     char dir[PATH_MAX];
@@ -402,7 +433,7 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
 	     tm->tm_sec,
 	     (int)(now.tv_nsec / 100000000));
     sp->fp = fopen(sp->filename,"w+");
-  }    
+  }
   if(sp->fp == NULL){
     fprintf(stderr,"can't create/write file %s: %s\n",sp->filename,strerror(errno));
     FREE(sp);
@@ -411,26 +442,26 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
   // file create succeded, now put us at top of list
   sp->prev = NULL;
   sp->next = Sessions;
-  
+
   if(sp->next)
     sp->next->prev = sp;
-  
+
   Sessions = sp;
 
   if(Verbose)
     fprintf(stdout,"creating %s\n",sp->filename);
-  
+
   sp->iobuffer = malloc(BUFFERSIZE);
   setbuffer(sp->fp,sp->iobuffer,BUFFERSIZE);
-  
+
   int const fd = fileno(sp->fp);
   fcntl(fd,F_SETFL,O_NONBLOCK); // Let's see if this keeps us from losing data
-  
+
   attrprintf(fd,"samplerate","%lu",(unsigned long)sp->samprate);
   attrprintf(fd,"channels","%d",sp->channels);
   attrprintf(fd,"ssrc","%u",rtp->ssrc);
   attrprintf(fd,"sampleformat","s16le");
-  
+
   // Write .wav header, skipping size fields
   memcpy(sp->header.ChunkID,"RIFF", 4);
   sp->header.ChunkSize = 0xffffffff; // Temporary
@@ -440,12 +471,27 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
   sp->header.AudioFormat = 1;
   sp->header.NumChannels = sp->channels;
   sp->header.SampleRate = sp->samprate;
-  
+
   sp->header.ByteRate = sp->samprate * sp->channels * 16/8;
   sp->header.BlockAlign = sp->channels * 16/8;
   sp->header.BitsPerSample = 16;
   memcpy(sp->header.SubChunk2ID,"data",4);
   sp->header.Subchunk2Size = 0xffffffff; // Temporary
+
+  // fill in the auxi chunk (start time, center frequency)
+  memcpy(sp->header.AuxID, "auxi", 4);
+  sp->header.AuxSize=164;
+  sp->header.StartYear=tm->tm_year+1900;
+  sp->header.StartMon=tm->tm_mon+1;
+  sp->header.StartDOW=tm->tm_wday;
+  sp->header.StartDay=tm->tm_mday;
+  sp->header.StartHour=tm->tm_hour;
+  sp->header.StartMinute=tm->tm_min;
+  sp->header.StartSecond=tm->tm_sec;
+  sp->header.StartMillis=(int16_t)(now.tv_nsec / 1000000);
+  sp->header.CenterFrequency=CenterFrequency;
+  memset(sp->header.AuxUknown, 0, 128);
+
   fwrite(&sp->header,sizeof(sp->header),1,sp->fp);
   fflush(sp->fp); // get at least the header out there
 
@@ -454,7 +500,7 @@ static struct session *create_session(struct rtp_header const *rtp,struct sockad
   getnameinfo((struct sockaddr *)sender,sizeof(*sender),sender_text,sizeof(sender_text),NULL,0,NI_NOFQDN|NI_DGRAM|NI_NUMERICHOST);
   attrprintf(fd,"source","%s",sender_text);
   attrprintf(fd,"multicast","%s",PCM_mcast_address_text);
-  
+
   attrprintf(fd,"unixstarttime","%ld.%09ld",(long)now.tv_sec,(long)now.tv_nsec);
   return sp;
 }
@@ -479,6 +525,20 @@ static int close_file(struct session **spp){
     }
     sp->header.ChunkSize = statbuf.st_size - 8;
     sp->header.Subchunk2Size = statbuf.st_size - sizeof(sp->header);
+
+    // write end time into the auxi chunk
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME,&now);
+    struct tm const * const tm = gmtime(&now.tv_sec);
+    sp->header.StopYear=tm->tm_year+1900;
+    sp->header.StopMon=tm->tm_mon+1;
+    sp->header.StopDOW=tm->tm_wday;
+    sp->header.StopDay=tm->tm_mday;
+    sp->header.StopHour=tm->tm_hour;
+    sp->header.StopMinute=tm->tm_min;
+    sp->header.StopSecond=tm->tm_sec;
+    sp->header.StopMillis=(int16_t)(now.tv_nsec / 1000000);
+
     rewind(sp->fp);
     fwrite(&sp->header,sizeof(sp->header),1,sp->fp);
     fflush(sp->fp);
