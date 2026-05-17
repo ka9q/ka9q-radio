6c978177492dda11d57f0d7f0862299b9d0f5823
Phil Karn <karn@ka9q.net>
2026-05-17 19:51:52 -0400

    clean deadwood; only create user/group radio in ka9q-radio-common

--- debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-ka9q-radio for Debian
---------------------
-
-<Possible notes regarding this package - if none, delete this file.>
-
- -- Phil Karn <karn@bart.ka9q.net>  Tue, 28 Apr 2026 10:36:52 -0700
