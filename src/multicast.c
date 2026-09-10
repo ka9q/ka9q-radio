5103c4d39ada0dd307d9a70ff8239ef1232d6483
Phil Karn <karn@ka9q.net>
2026-09-09 19:10:15 -0700

    debug tracing for wait_for_lan

--- src/multicast.c
+++ src/multicast.c
@@ -931,6 +931,13 @@ bool wait_for_lan(void){
       return false;
 
     for(struct ifaddrs const *p = ifa; p != NULL; p = p->ifa_next) {
+      if(p->ifa_addr == NULL)
+	continue;
+#if 1
+      fprintf("family %d loopback %d up %d multicast %d\n",
+	      p->ifa_addr->sa_family,p->ifa_flags & IFF_LOOPBACK,
+	      p->ifa_flags & IFF_UP, p->ifa_flags & IFF_MULTICAST);
+#endif
       if(p->ifa_addr != NULL && p->ifa_addr->sa_family == AF_INET && !(p->ifa_flags & IFF_LOOPBACK) 
 	 && (p->ifa_flags & IFF_UP) && (p->ifa_flags & IFF_MULTICAST)){
 	LAN_found = true;
