6a18a15e38894fc038b4d3402316676207b152ec
Phil Karn <karn@ka9q.net>
2026-09-13 19:04:52 -0700

    more hidden dependencies

--- src/defaults.h
+++ src/defaults.h
@@ -5,6 +5,7 @@
 #ifndef _DEFAULTS_H
 #define _DEFAULTS_H 1
 #include "window.h"
+#include "status.h"
 #define Nchannels 2000                          // Maximum number of channels (static array)
 
 static int const DEFAULT_PRIO = 1;              // Default real-time priority for channel thread (Linux only);
